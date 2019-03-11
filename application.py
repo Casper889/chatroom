03bb70b768216f1073158472cf463dee82061bea
Casper <casperq852@gmail.com>
2019-03-11 16:26:21 +0100

    Made layout and index.html and basic homepage

--- application.py
+++ application.py
@@ -1,13 +1,28 @@
 import os
-
-from flask import Flask
-from flask_socketio import SocketIO, emit
+import datetime
+from flask import Flask, render_template, redirect, jsonify, request, url_for, session
+from flask_socketio import SocketIO, emit, send
+from flask_session import Session
+from channels import Channel
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
 
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+
+socketio = SocketIO(app)
+Session(app)
+channels = []
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    """display home page and check for user"""
+    try:
+        return render_template("index.html", name=session["name"], lastChannel=session["lastChannel"], channels=channels)
+    except KeyError:
+        try:
+            return render_template("index.html", name=session["name"], channels=channels)
+        except KeyError:
+            return render_template("index.html", channels=channels)
